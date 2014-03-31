2e4113bcdec1fc5f5157a2869bb7d76394c05873
wondershow@gmail.com <wondershow@gmail.com@a58fe4a8-e837-3e8a-57e5-934cd0cb282d>
2014-03-31 23:53:10 +0000

    just for test

--- trunk/src/bestroomie/controller/BRAbstractController.java
+++ trunk/src/bestroomie/controller/BRAbstractController.java
@@ -6,6 +6,7 @@ import bestroomie.entities.BRAbstractEntity;
 
 public abstract class BRAbstractController implements ActionListener{
 	
+	//lei added code
 	//When business on this controller/view is finished, specify which controller/view to go
 	protected abstract void goToController(BRAbstractEntity e, BRAbstractController b);
 	
