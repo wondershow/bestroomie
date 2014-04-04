4b47b65a524002d051bc4b1d1cd2e0045d12a5fc
wondershow@gmail.com <wondershow@gmail.com@a58fe4a8-e837-3e8a-57e5-934cd0cb282d>
2014-04-04 18:54:28 +0000

    demo

---
+++ trunk/src/bestroomie/gui/SVNDemo.java
@@ -0,0 +1,18 @@
+/**
+Author: Lei Zhang
+File Creation Date: Apr 4, 2014
+Class Description:
+*/
+package bestroomie.gui;
+public class SVNDemo {
+	
+	private String userAge() {
+		return "name";
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
