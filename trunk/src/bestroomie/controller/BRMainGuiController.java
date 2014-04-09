314c808dd76e4d36e88f32faa3ee1c617dc81cb5
wondershow@gmail.com <wondershow@gmail.com@a58fe4a8-e837-3e8a-57e5-934cd0cb282d>
2014-04-09 14:19:19 +0000

    Lei Zhang commit

--- trunk/src/bestroomie/controller/BRMainGuiController.java
+++ trunk/src/bestroomie/controller/BRMainGuiController.java
@@ -3,16 +3,17 @@ package bestroomie.controller;
 import java.awt.event.ActionEvent;
 
 import bestroomie.entities.BRAbstractEntity;
-import bestroomie.entities.BRGroup;
-import bestroomie.entities.BRUser;
-import bestroomie.gui.BRAbstractView;
-import bestroomie.gui.BRMainGui;
+import bestroomie.entities.*;
+import bestroomie.gui.*;
 
 public class BRMainGuiController extends BRAbstractController {
 
 	private BRUser user;
 	private BRGroup grp;
-	private BRAbstractView mainFrame;
+	private BRMainGui mainFrame;
+	private	BRHeadPanelController headController;
+	private BRGoupPanelController groupController;
+	private BRGroupPanel userPanel;
 	
 	
 	public void refreshUI() {
@@ -20,6 +21,24 @@ public class BRMainGuiController extends BRAbstractController {
 		this.mainFrame.setResizable(false);
 		this.mainFrame.setLocationRelativeTo(null);
 		this.mainFrame.setVisible(true);
+		System.out.println("I am here!!!");
+		headController.setUpView();
+		groupController.setupGroupLists(this.user.getFirstGrpId());
+	}
+
+	/**
+	 * To update the selected group by passing 
+	 * group id
+	 * **/
+	public void setSelectedGrp(String grpId) {
+		grp = new BRGroup(grpId);
+		grp.load();
+	}
+	
+	/**
+	 ***/
+	public BRGroup getSelectedGrp(){
+		return this.grp;
 	}
 	
 	
@@ -27,9 +46,25 @@ public class BRMainGuiController extends BRAbstractController {
 		this.user = u;
 		BRMainGui gui = new BRMainGui();
 		this.mainFrame = gui;
-		this.refreshUI();
+		
+		
+		this.setSelectedGrp(this.user.getFirstGrpId());
+		headController = new BRHeadPanelController(this.user,this.mainFrame.getHeadPanel(),this);
+		groupController = new BRGoupPanelController(this.user,this.mainFrame.getGroupPanel(),this);
+		
+		
+		
+		
+		//this.refreshUI();
 	}
 	
+	
+	
+	public void setupGUI() {
+		
+		
+		
+	}
 	
 	
 	@Override
@@ -44,7 +79,7 @@ public class BRMainGuiController extends BRAbstractController {
 		
 	}
 	
-	public BRMainGuiController(BRUser u,BRAbstractView v) {
+	public BRMainGuiController(BRUser u,BRMainGui v) {
 		this.user = u;
 		this.mainFrame = v;
 		this.mainFrame.setVisible(true);
