e7520af53da3d60b914aa14391481c9b01c3bd8c
wondershow@gmail.com <wondershow@gmail.com@a58fe4a8-e837-3e8a-57e5-934cd0cb282d>
2014-04-11 01:00:56 +0000

    Change the display of group panel by using JList instead of using JButton

--- trunk/src/bestroomie/gui/BRGroupPanel.java
+++ trunk/src/bestroomie/gui/BRGroupPanel.java
@@ -5,13 +5,14 @@ Class Description:
 */
 package bestroomie.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 import bestroomie.controller.*;
 import bestroomie.util.BRConst;
@@ -41,22 +42,63 @@ public class BRGroupPanel extends BRMainPanel {
 	 *  be displayed in a different color 
 	 * **/
 	public void setupGroupLists(String groups[], String selectedGrp) {
-		this.removeAll();
-		this.repaint();
 		
-		
-//		System.out.println("Selected group is " + selectedGrp);
-		int grpNum = groups.length;
-		this.setLayout(new GridLayout(grpNum,1));
-		for (int i=0;i<grpNum;i++) {
-			JButton btn = new JButton(groups[i]);
-			if(groups[i].equals(selectedGrp)) {
-//				System.out.println("We are adding " + selectedGrp );
-				btn.setBackground(new Color(255,0,0));
+		/**The following code realize the groupname display using JButton**/
+		if(false) { 
+			this.removeAll();
+			this.repaint();
+			
+			
+	//		System.out.println("Selected group is " + selectedGrp);
+			int grpNum = groups.length;
+			this.setLayout(new GridLayout(grpNum,1));
+			for (int i=0;i<grpNum;i++) {
+				JButton btn = new JButton(groups[i]);
+				if(groups[i].equals(selectedGrp)) {
+	//				System.out.println("We are adding " + selectedGrp );
+					btn.setBackground(new Color(255,0,0));
+				}
+				this.add(btn);
 			}
-			this.add(btn);
+			this.revalidate();
+		} else {
+			this.removeAll();
+			this.repaint();
+			int grpNum = groups.length;
+			this.setLayout( new BorderLayout(  ));
+			JScrollPane scrollPane = new JScrollPane();
+			JList<String> gNameList = new JList<String>(groups);
+			gNameList.setPreferredSize(new Dimension(this.groupPanelWidth*2/3, 
+									   this.groupPanelHeight/2));
+			
+			/** TODO set fonts**/
+			//Font f = gNameList.getFont();
+			//gNameList.setFont(new Font(f.getName(), Font.ITALIC,3*f.getSize()));
+			
+			gNameList.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
+			
+			int selectedIndex = -1;
+			for(int i=0;i<groups.length;i++)
+				if(groups[i].equals(selectedGrp))
+					selectedIndex = i;
+			gNameList.setSelectedIndex( selectedIndex );
+			
+			
+			
+			
+			
+//			scrollPane.setViewportView(gNameList);
+//			gNameList.setf
+//			gNameList.setSelectedIndex(index);
+//			for (int i=0;i<grpNum;i++) {
+//				JRadioButton radioBtn = new JRadioButton(groups[i]);
+//				group.add(radioBtn);
+//				this.add(radioBtn);
+//			}
+			//System.out.println(gNameList.get)
+			this.add(new JScrollPane(gNameList));
+			this.revalidate();
 		}
-		this.revalidate();
 	}
 
 	@Override
@@ -65,10 +107,10 @@ public class BRGroupPanel extends BRMainPanel {
 //		System.out.println("I am here registering");
 		for (Component c : this.getComponents())
 		{
-		      if (c instanceof JButton)
+		      if (c instanceof JRadioButton)
 		      {
 //		    	   System.out.println("I am reg listener in the group panel, the has code of the traget is " + a.toString());
-		           JButton tmp = (JButton)c;
+		    	  JRadioButton tmp = (JRadioButton)c;
 		           tmp.addActionListener(a);
 		      }
 		 }
