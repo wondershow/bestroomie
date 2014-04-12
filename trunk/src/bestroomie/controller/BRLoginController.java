c1eda8d5521cb6c519d1189035284e12b4e057af
Matt.Svensson <Matt.Svensson@a58fe4a8-e837-3e8a-57e5-934cd0cb282d>
2014-04-12 21:03:35 +0000

    removed initial "main GUI" view display after logon

--- trunk/src/bestroomie/controller/BRLoginController.java
+++ trunk/src/bestroomie/controller/BRLoginController.java
@@ -28,7 +28,6 @@ public class BRLoginController extends BRAbstractController {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Fuck putin: !!!" + e.getActionCommand()+"111");
 		if(e.getActionCommand().equals("Login")) {
 			
 			if(!this.validateCaptcha())
@@ -45,11 +44,6 @@ public class BRLoginController extends BRAbstractController {
 					v.registerListener(c);
 					v.setVisible(true);	
 				}
-					
-				//Go to MainUI
-				this.view.dispose();
-				BRMainGuiController mainGui = new BRMainGuiController(this.model);
-				mainGui.refreshUI();
 			}
 			
 		} else if (e.getActionCommand().equals("Cancel")) {
